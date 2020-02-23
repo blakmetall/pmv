2cb879464ca6cec49aebb43a1d892ba432cc69be
Jose Faro <blakmetall@gmail.com>
2020-02-22 18:40:45 -0600

    fix: add https for asset generations for production and staging environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // database mysql schema for strings
         Schema::defaultStringLength(191);
+
+        // https configuration
+        $https_environments = ['production', 'staging'];
+        if( in_array(config('app.env'), $https_environments) ) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
