de13c995ec7bb5a7c15efa74bbf01d4fbddbc06e
Jose Faro <blakmetall@gmail.com>
2020-02-23 15:08:07 -0600

    feat: react integration for later shared components

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,8 @@
  * building robust, powerful web applications using React + Laravel.
  */
 
-require('./bootstrap');
+// already loading bootstrap js withing the theme
+// require('./bootstrap');
 
 /**
  * Next, we will create a fresh React component instance and attach it to
