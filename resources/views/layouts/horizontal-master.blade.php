907a5b38fa2e607fbe240c203224953d3a919acd
Jose Faro <blakmetall@gmail.com>
2020-04-25 16:54:40 -0500

    feat: stepper first revision

--- resources/views/layouts/horizontal-master.blade.php
+++ resources/views/layouts/horizontal-master.blade.php
@@ -18,6 +18,9 @@
         <!-- css before theme -->
         @yield('before-css')
 
+        {{-- smart wizard --}}
+        <link rel="stylesheet" href="{{asset('assets/styles/vendor/smart.wizard/smart_wizard.min.css')}}">
+
         {{-- theme css --}}
         <link id="gull-theme" rel="stylesheet" href="{{  asset('assets/styles/css/themes/palmera-vacations.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/styles/vendor/perfect-scrollbar.css') }}">
@@ -83,6 +86,9 @@
 
         {{-- page specific javascript --}}
         @yield('page-js')
+
+        {{-- smart wizard --}}
+        <script src="{{asset('assets/js/vendor/jquery.smartWizard.min.js')}}"></script>
 
         {{-- theme javascript --}}
         <script src="{{asset('assets/js/script.js')}}"></script>
---
+++ resources/views/partials/stepper.blade.php
@@ -0,0 +1,12 @@
+<div class="row mb-4">
+    <div class="col-md-12">
+        <div id="smartwizard">
+            <ul>
+                <li><a href="#step-1">Step 1<br /><small>This is step description</small></a></li>
+                <li><a href="#step-2">Step 2<br /><small>This is step description</small></a></li>
+                <li><a href="#step-3">Step 3<br /><small>This is step description</small></a></li>
+                <li><a href="#step-4">Step 4<br /><small>This is step description</small></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
