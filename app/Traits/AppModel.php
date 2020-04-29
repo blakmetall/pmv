601b257dcb98b5a93b6be353887c608ed913cf1d
José Faro | Home <blakmetall@gmail.com>
2020-04-29 10:02:57 -0500

    feat: update trait to get register translation methods to models

--- app/Traits/AppModel.php
+++ app/Traits/AppModel.php
@@ -2,10 +2,22 @@
 
 namespace App\Traits;
 
+use App\Helpers\LanguageHelper;
+
 trait AppModel {
 
     public function _getTable() { 
         return $this->table; 
     }
 
+    public function hasTranslation() {
+        $language_id = LanguageHelper::getId( LanguageHelper::getLocale() );
+        return $this->translations()->where('language_id', $language_id)->count();
+    }
+
+    public function translate() {
+        $language_id = LanguageHelper::getId( LanguageHelper::getLocale() );
+        return $this->translations()->where('language_id', $language_id)->first();
+    }
+    
 }
