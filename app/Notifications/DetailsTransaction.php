ef39b186a7ad8a3262ab3dd9fdb78db3ef157d14
José Faro | Home <blakmetall@gmail.com>
2020-10-24 21:19:00 -0500

    prueba de correo 7: transacciones

--- app/Notifications/DetailsTransaction.php
+++ app/Notifications/DetailsTransaction.php
@@ -2,11 +2,10 @@
 
 namespace App\Notifications;
 
+use App\Models\PropertyManagementTransaction;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use App\Models\PropertyManagementTransaction;
 
 class DetailsTransaction extends Notification
 {
@@ -25,7 +24,8 @@ class DetailsTransaction extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
+     *
      * @return array
      */
     public function via($notifiable)
@@ -36,35 +36,36 @@ class DetailsTransaction extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
+     *
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
-        $greeting = sprintf('Hello %s!', $notifiable->profile->full_namee);
-        $period = $this->transaction->period_start_date . ' - ' . $this->transaction->period_end_date;
+        $greeting = sprintf('Hello %s!', $notifiable->profile->full_name);
+        $period = $this->transaction->period_start_date.' - '.$this->transaction->period_end_date;
 
-        return (new MailMessage)
-            ->subject(__('Detail of Transaction') . $this->transaction->id)
+        return (new MailMessage())
+            ->subject(__('Transaction').': '.$this->transaction->id)
             ->greeting($greeting)
-            ->line('This are the details for transaction')
-            ->line(__('Amount') . $this->transaction->amount)
-            ->line(__('Period') . $period)
-            ->line(__('Type') . $this->transaction->type->translate()->name)
-            ->line(__('Post Date') . $this->transaction->post_date)
-            ->line('Thank you for using our application!');
+            ->line(new HtmlString(__('Property').': '.'<strong>'.$this->data->property.'</strong>'))
+            ->line(new HtmlString(__('Post Date').': '.'<strong>'.$this->transaction->post_date.'</strong>'))
+            ->line(new HtmlString(__('Description').': '.$this->transaction->description))
+            ->line(new HtmlString(__('Period').': '.'<strong>'.$period.'</strong>'))
+            ->line(new HtmlString(__('Operation Type').': '.'<strong>'.$this->transaction->type->translate()->name.'</strong>'))
+            ->line(new HtmlString(__('Amount').': '.'<strong>'.priceFormat($this->transaction->amount).'</strong>'));
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
+     *
      * @return array
      */
     public function toArray($notifiable)
     {
         return [
-            //
         ];
     }
 }
