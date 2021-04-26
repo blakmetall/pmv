49752b14972bb04c28c7a8f09b202828138e2faa
Jose <blakmetall@gmail.com>
2021-04-26 17:44:14 -0500

    integración de todas las opciones de menu de settings en su pagina de blade a través del controlador de Settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,52 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\RoleHelper;
+
 class SettingsController extends Controller
 {
     public function index() 
     {
-        $menu = [
-            [
-                'label' => __('Users'),
-                'url'=> route('users')
-            ],
-            [
-                'label' => __('Workgroups'),
-                'url'=> route('workgroups')
-            ],
-            [
-                'label' => __('Roles'),
-                'url'=> route('roles')
-            ],
-            [
-                'label' => __('Cities'),
-                'url'=> route('cities')
-            ],
-            [
-                'label' => __('Zones'),
-                'url' => route('zones')
-            ],
-            [
-                'label' => __('Contacts'),
-                'url'=> route('contacts')
-            ],
-            [
-                'label' => __('Amenities'),
-                'url' => route('amenities')
-            ],
-            [
-                'label' => __('Transaction Types'),
-                'url' => route('transaction-types')
-            ],
-            [
-                'label' => __('Cleaning Options'),
-                'url' => route('cleaning-options')
-            ],
-            [
-                'label' => __('Damage Deposits'),
-                'url' => route('damage-deposits')
-            ],
-        ];
+        $_current_role = RoleHelper::current();
+
+        $menu = [];
+
+        if ($_current_role->isAllowed('settings', 'users')) {
+            $menu[] = ['label' => __('Users'), 'url'=> route('users')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'workgroups')) {
+            $menu[] = ['label' => __('Workgroups'), 'url'=> route('workgroups')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'roles')) {
+            $menu[] = ['label' => __('Roles'), 'url'=> route('roles')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'cities')) {
+            $menu[] = ['label' => __('Cities'), 'url'=> route('cities')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'offices')) {
+            $menu[] = ['label' => __('Offices'), 'url'=> route('offices')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'zones')) {
+            $menu[] = ['label' => __('Zones'), 'url'=> route('zones')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'buildings')) {
+            $menu[] = ['label' => __('Buildings'), 'url'=> route('buildings')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'contacts')) {
+            $menu[] = ['label' => __('Contacts'), 'url'=> route('contacts')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'amenities')) {
+            $menu[] = ['label' => __('Amenities'), 'url'=> route('amenities')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'transaction-types')) {
+            $menu[] = ['label' => __('Transaction Types'), 'url'=> route('transaction-types')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'cleaning-options')) {
+            $menu[] = ['label' => __('Cleaning Options'), 'url'=> route('cleaning-options')];
+        }
+
+        if ($_current_role->isAllowed('settings', 'damage-deposits')) {
+            $menu[] = ['label' => __('Damage Deposits'), 'url'=> route('damage-deposits')];
+        }
         
         return view('settings.index')->with('menu', $menu);
     }
