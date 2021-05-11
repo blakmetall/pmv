eafb455bdad11bb567d866525dab0e170119d14e
Alan Cordova <info.devalan@gmail.com>
2021-05-11 03:16:31 -0500

    Se hicieron los ajusted y correciones del issue de github Reservation Email send incomplete - Multiple things #59

--- app/Http/Controllers/PropertyBookingController.php
+++ app/Http/Controllers/PropertyBookingController.php
@@ -531,7 +531,8 @@ class PropertyBookingController extends Controller
             return redirect()->back();
         } else {
             $booking = $this->repository->create($request);
-            $request->session()->flash('success', __('Record created successfully'));
+            $msg = __('Msg reservation') . ' #'.$booking->id.', '.__('Client wishes').' <a href="insertarlink">'.__('Click here').'</a> '.__('Confirmation recipents').'reservaciones@palmeravacations.com, info@palmeravacations.com, contabilidad@palmeravacations.com';
+            $request->session()->flash('success', $msg);
             return redirect(route('property-bookings.edit', [$booking->id]));
         }
     }
