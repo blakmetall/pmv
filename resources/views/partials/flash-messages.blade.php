664ba3a6faf0305a7c71e26bae7ea83704ed4f79
Alan Cordova <info.devalan@gmail.com>
2021-08-01 12:36:44 -0500

    Detalle query occupancy

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -10,7 +10,7 @@
     <div class="app-container app-centered">
         <div class="alert alert-danger app-alert-danger mb-4" role="alert">
             {!! Session::get('error') !!} 
-            @if($_GET['adults_sing']) 
+            @if(isset($_GET['adults_sing'])) 
                 - <a href="{{ route('public.reservations', [App::getLocale(), $property->property_id]) }}" class="btn-send-query">{{ __('Send Query') }}
                 </a>
             @endif
