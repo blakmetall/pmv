be02d9a004071e3d3835c4864cd024f1f9d572aa
Alan Cordova <info.devalan@gmail.com>
2021-09-22 23:45:32 -0500

    SEO Dates Property

--- resources/views/layouts/public-master.blade.php
+++ resources/views/layouts/public-master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    {!! SEO::generate() !!}
+    {!! SEO::generate(true) !!}
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
